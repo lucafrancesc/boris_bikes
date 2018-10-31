73e7df5eb4750e93b37c1d0cbe726af986b98b6f
Luca <eto.lucafrancesco@gmail.com>
2018-10-31 15:48:21 +0000

    Docking method created.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,13 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike}
 
-  it 'expected to return a working bike' do
+  it 'expects to return a working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'expects to dock a bike' do
+    expect(subject).to respond_to(:dock).with(1).argument
   end
 
 end
