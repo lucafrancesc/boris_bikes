0bc6883bbfee6562684282337aa20efefbeb738b
Luca <eto.lucafrancesco@gmail.com>
2018-11-01 15:38:35 +0000

    docking station full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,11 +5,12 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-   fail 'No bikes available' unless @bike
-   @bike
+     fail "No bikes available" unless @bike
+     @bike
  end
 
   def dock(bike)
+    fail "Docking Station full" if @bike
     @bike = bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,13 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike}
+  it 'expects to respond to #bike' do
+    expect(subject).to respond_to(:bike)
+  end
 
   describe '#release_bike' do
+
+    it { is_expected.to respond_to :release_bike}
 
     it 'releases a bike' do
       bike = Bike.new
@@ -15,26 +19,34 @@ describe DockingStation do
     it 'raises an error when no bikes to release' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
       end
+
   end
 
 
-  it 'expects to respond a #dock with 1 arg' do
-    expect(subject).to respond_to(:dock).with(1).argument
+  describe '#dock' do
+
+
+    it 'expects to respond a #dock with 1 arg' do
+      expect(subject).to respond_to(:dock).with(1).argument
+    end
+
+    it 'expects to store an instance of Bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'expects to return an instance of bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+
+    it 'raises an error when docking station is full' do
+      subject.dock(Bike.new)
+      expect { subject.dock(Bike.new) }.to raise_error 'Docking Station full'
+    end
+
   end
 
-  it 'expects to respond to #bike' do
-    expect(subject).to respond_to(:bike)
-  end
-
-  it 'expects to store an instance of Bike' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'expects to return an instance of bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
 
 end
